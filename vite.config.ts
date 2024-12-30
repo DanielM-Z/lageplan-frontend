60d4e1d7e7ec5e63380a54e80c7da67072f4d43d
PatrichHeadNToes <pomtier123@gmail.com>
2024-12-30 16:43:52 +0100

    Added leaflet to vite.config. hopefully to solve Leaflet.Editable.js issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['leaflet', 'leaflet.path.drag']
+	},
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
 	ssr: {
 		noExternal: ['@googlemaps/js-api-loader']
 	},
